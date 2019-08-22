21aaebcddb6841c15b0abd445ac1601c6f89df27
frpericacho <frpericacho@gmail.com>
2019-08-22 19:07:18 +0200

    :rice_ball:

--- Practicas/src/main/java/ejerciciosexamen/febrero1314/analisisRendimiento.cpp
+++ Practicas/src/main/java/ejerciciosexamen/febrero1314/analisisRendimiento.cpp
@@ -1,48 +1,59 @@
-#include <iostream>
 #include <atomic>
+#include <chrono>
+#include <ctime>
+#include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
-#include <chrono>
-#include <ctime>
 
-class analisisRendimiento{
-    public:
-    std::mutex mutex;
-    int val;
-    std::atomic<int> val2;
+class analisisRendimiento {
+ public:
+  std::mutex mutex;
+  int val;
+  std::atomic<int> val2;
 
-    void fun_mutex(){
-        mutex.lock();
-        val++;
-        mutex.unlock();
-    }
+  void fun_mutex() {
+    mutex.lock();
+    val++;
+    mutex.unlock();
+  }
 
-    void fun_atom(){
-        val2++;
-    }
+  void fun_atom() { val2++; }
+};
 
-    void main(){
-        std::vector<std::thread> hilos;
+int main() {
+  std::vector<std::thread> hilos;
+  analisisRendimiento cont;
 
-        std::chrono::time_point<std::chrono::system_clock> start, end;
-        start = std::chrono::system_clock::now();
-        fun_mutex();
-        end = std::chrono::system_clock::now();
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  start = std::chrono::system_clock::now();
+  for (int j = 0; j < 10; j++) {
+    hilos.push_back(std::thread([&cont]() {
+      for (int i = 0; i < 10; i++) cont.fun_mutex();
+    }));
+  }
+  for (int i = 0; i < 10; i++) hilos[i].join();
+  end = std::chrono::system_clock::now();
 
-        std::chrono::duration<double> elapsed_seconds = end-start;
-        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-        std::cout << "Calculo terminado a las: " << std::ctime(&end_time)
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+  std::cout << "Calculo terminado a las: " << std::ctime(&end_time)
             << "Tiempo: " << elapsed_seconds.count() << "s\n";
 
-        std::chrono::time_point<std::chrono::system_clock> start2, end2;
-        start2 = std::chrono::system_clock::now();
-        fun_atom();
-        end2 = std::chrono::system_clock::now();
+  std::chrono::time_point<std::chrono::system_clock> start2, end2;
+  start2 = std::chrono::system_clock::now();
+  for (int j = 0; j < 10; j++) {
+    hilos.push_back(std::thread([&cont]() {
+      for (int i = 0; i < 10; i++) cont.fun_atom();
+    }));
+  }
+  for (int i = 0; i < 10; i++) hilos[i].join();
+  end2 = std::chrono::system_clock::now();
 
-        std::chrono::duration<double> elapsed_seconds2 = end2-start2;
-        std::time_t end_time2 = std::chrono::system_clock::to_time_t(end2);
-        std::cout << "Calculo terminado a las: " << std::ctime(&end_time2)
+  std::chrono::duration<double> elapsed_seconds2 = end2 - start2;
+  std::time_t end_time2 = std::chrono::system_clock::to_time_t(end2);
+  std::cout << "Calculo terminado a las: " << std::ctime(&end_time2)
             << "Tiempo: " << elapsed_seconds2.count() << "s\n";
-    }
-};
+
+  return 0;
+}
